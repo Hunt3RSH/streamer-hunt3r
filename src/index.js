6467109f1401c3a256436b8ac5c3d13cb962880f
Volodymyr Shamrai <s.v.777.energy@gmail.com>
2024-12-23 01:59:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { theme } from 'utils/theme';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+          <BrowserRouter basename="streamer-hunt3r">
+            <App />
+          </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
