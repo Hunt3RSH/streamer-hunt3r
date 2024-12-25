9d71a0bf8b642f255136dd7725907f7438428313
Volodymyr Shamrai <s.v.777.energy@gmail.com>
2024-12-25 16:03:50 +0200

    fix

--- src/views/Rules/index.jsx
+++ src/views/Rules/index.jsx
@@ -29,7 +29,7 @@ export default function Rules() {
               <h3 className="text-sm font-medium text-gray-900">Загальні</h3>
 
               <div className="mt-4">
-                <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
+                <ul className="list-disc space-y-2 pl-4 text-sm">
                   {product.highlights.map((highlight) => (
                     <li key={highlight} className="text-gray-400">
                       <span className="text-gray-600">{highlight}</span>
